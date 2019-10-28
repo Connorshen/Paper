8a8691272afdc4ab64f2dd065ac719bda9907fe0
billshen <914138410@qq.com>
2019-10-28 19:11:04 +0800

    add

--- ver3/set_activity_CPL.m
+++ ver3/set_activity_CPL.m
@@ -2,22 +2,10 @@ function output_CPL = set_activity_CPL(input_CPL, weight_recurrent_CPL, para)
 
 numNeurons_CPL = para(1);
 numNeurons_cluster = para(2);
-flag_sparse = para(3);
-diff_th = para(4);
-
 output_CPL = zeros(size(input_CPL));
 % 最大值置为1，其余为0
 for i = 1:numNeurons_cluster:numNeurons_CPL
-    
     ind_cluster = weight_recurrent_CPL(i:i+numNeurons_cluster-1);
-    [val_sorted, ind_sorted] = sort(input_CPL(ind_cluster), 'descend');
-    
-    if flag_sparse
-        % sparse according to the diff between the max and the second val
-        if val_sorted(1)-val_sorted(2) > diff_th
-            output_CPL(ind_cluster(ind_sorted(1))) = 1;
-        end
-    else
-        output_CPL(ind_cluster(ind_sorted(1))) = 1;
-    end
+    [~, ind_max] = max(input_CPL(ind_cluster));
+    output_CPL(ind_cluster(ind_max)) = 1;
 end
--- ver3/set_parameters.m
+++ ver3/set_parameters.m
@@ -5,10 +5,9 @@ function  init_para = set_parameters()
 init_para.num_rounds = 1000;
 init_para.trials_round = 60;
 init_para.flag_herg_inputCPL = true;
-init_para.flag_sparse = true;
-init_para.flag_saveresult = false;
+init_para.flag_saveresult = true;
 
-init_para.digit_label = [0:9]; 
+init_para.digit_label = 0:9; 
 
 init_para.numNeurons_input = 2560;
 init_para.numNeurons_CPL = 200000;
@@ -16,7 +15,7 @@ init_para.numNeurons_cluster = 10;
 init_para.numNeurons_decision = 10;
 
 init_para.prob_input_CPL = 0.01;
-init_para.hprob_input_CPL = [0.001:0.001:0.01];
+init_para.hprob_input_CPL = 0.001:0.001:0.01;
 
 init_para.diff_th = 0.1;
 init_para.learning_rate = 0.5;
