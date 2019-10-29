229de086c41d7f5a8deea3fe7def4dc14b519d58
billshen <914138410@qq.com>
2019-10-29 16:22:22 +0800

    add

--- ver3/testing_trained.m
+++
@@ -1,8 +0,0 @@
-
-clear all
-load('results_trained_0_9_p98-35.mat', 'results')
-
-network_trained = results.network_trained;
-init_para = results.init_para;
-
-testing_result = run_testing( network_trained, init_para);
