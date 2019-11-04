4e0c9a3b19411eee81f9c93be3e01137bbe08a35
billshen <914138410@qq.com>
2019-11-04 14:30:36 +0800

    add

--- url.py
+++ url.py
@@ -7,7 +7,7 @@ class DataConfig:
     ROOT = path.dirname(path.realpath(__file__))
     DATA_PATH = path.join(ROOT, "data")
     MNIST_PATH = path.join(DATA_PATH, "mnist")
-    FASHION_PATH = path.join(DATA_PATH, "fashion-mnist")
+    FASHION_PATH = path.join(DATA_PATH, "fashion_mnist")
     MNIST_TRAIN_IMAGE_PATH = path.join(MNIST_PATH, "train-images-idx3-ubyte")
     MNIST_TRAIN_LABEL_PATH = path.join(MNIST_PATH, "train-labels-idx1-ubyte")
     MNIST_TEST_IMAGE_PATH = path.join(MNIST_PATH, "t10k-images-idx3-ubyte")
