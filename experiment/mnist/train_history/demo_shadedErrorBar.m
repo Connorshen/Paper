91b419a2bf8138e85c7078fce7662b1e055bb3c7
billshen <914138410@qq.com>
2019-11-20 11:51:31 +0800

    add

--- experiment/mnist/train_history/demo_shadedErrorBar.m
+++
@@ -1,3 +0,0 @@
-y=randn(30,80); %随机生成30行80列的数据
-x=1:size(y,2); % x 为shadeErrorBar的横轴
-shadedErrorBar(x,mean(y,1),std(y),'lineprops','g');%参数分别是，横轴刻度值，y的均值，y的标准差，配置线条的颜色为绿色
---
+++ experiment/mnist/training_cluster_size.m
@@ -0,0 +1,36 @@
+clear
+clc
+% 无关的参数
+trial = 3;
+data_ratio = 1;% 数据集比例
+test_early_stopping = -1;% 测试的时候提早break的step，不想提早结束的话取-1
+train_early_stopping = 1000;% 训练的时候提早break的step，不想提早结束的话取-1
+file_name = "train_history/compare_cluster_size.mat";
+compare_cluster_size = cell(1,2);% [rl_check_points,rl_batch_check_points]
+% 初始化参数
+digits = 0:2;
+in_features_cpl = 2560;
+out_features_cpl = 900;
+verify_step = 100;
+get_lr_step = 100;
+get_lr_batch = 100;
+n_neuron_clusters = [2,5,10,15,30];
+% 加载数据集
+data = load_mnist_data(digits,data_ratio);
+disp("load data success");
+for i=1:length(n_neuron_clusters)
+    n_neuron_cluster = n_neuron_clusters(i);
+    for j=1:trial
+        init_para = init_paramter(digits,in_features_cpl,out_features_cpl,n_neuron_cluster,verify_step,get_lr_step,get_lr_batch);
+        disp("init para success")
+        init_para
+        rand('state',j);
+        net = init_net(init_para);
+        disp("init net success")
+        [rl_check_points,rl_best_train_result] = rl_trainer(init_para,net,data,train_early_stopping,test_early_stopping);
+        compare_cluster_size{i,j*2-1} = rl_check_points;
+        compare_cluster_size{i,j*2} = rl_best_train_result.init_para.n_neuron_cluster;
+    end
+end
+save(file_name,"compare_cluster_size","-v7.3");
+plot_out_features()
