71fe0eee606fddee8f808ace0b7c102a197698ab
billshen <914138410@qq.com>
2019-11-27 20:07:44 +0800

    add

---
+++ experiment/mnist/train_history/get_final_acc.m
@@ -0,0 +1,13 @@
+function acc_final = get_final_acc()
+load("compare_final_acc")
+n_trial = size(compare_final_acc,1);
+acc_max_all = [];
+for i = 1:n_trial
+    step_all = compare_final_acc{i,1}(:,1);
+    step_index = step_all(compare_final_acc{i,1}(:,4)~=0);
+    acc_all = compare_final_acc{i,1}(:,4);
+    acc_all = acc_all(step_index);
+    acc_max = max(acc_all);
+    acc_max_all = [acc_max_all;acc_max];
+end
+acc_final = max(acc_max_all);
